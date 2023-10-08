58ac6d5bfe443a7d58aa04ee1a1359ef3aaf1d87
CHENNAM SETTY RAMA CHARAN <106875235+RAMACHARAN-GIT@users.noreply.github.com>
2023-10-08 20:43:29 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
-class Main{
+public class Main{
+    int i = 10;
     public static void main(String[] args){
+        Main my_Object = new Main();
+        System.out.println(my_Object.i);
         System.out.println("Hello world");
     }
 }
